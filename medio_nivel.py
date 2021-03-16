3c7ce6b7f860eaa404dfae215b26b39e25868449
manuel mariscal <i72marom@uco.es>
2021-03-16 15:14:25 +0100

    overflow

--- medio_nivel.py
+++ medio_nivel.py
@@ -7,13 +7,16 @@ import random
 def escribeFichero(lista, nombre):
 	f = open("txt/" + nombre + ".txt", "w")
 	
+	# primera columna = numero de elementos
+	# segunda colunna = tiempo interprete
+	# tercera colunna = tiempo numba
 	for i in lista: 
-		f.write(str('{:,.15f}'.format(i)) + "\n")
+		f.write(str(i[0]) + " "+ str('{:,.15f}'.format(i[1])) + " "+ str('{:,.15f}'.format(i[2])) + "\n")
 
 	f.close()
 
 def imprimeAyuda():
-	ayuda_ = "Opciones: \n\t0. Salir. \n\t1. Ayuda. \n\t2. Determinante iterativo.\n\t3. Determinante recursivo.\n"
+	ayuda_ = "Opciones: \n\t0. Salir. \n\t1. Muestra este menú. \n\t2. Determinante iterativo.\n\t3. Determinante recursivo.\n"
 	print(ayuda_)
 
 def menu():
@@ -26,24 +29,22 @@ def menu():
 
 @numba.jit
 def rellenaMatriz(x):
-	return numpy.random.uniform(-10.0, 10.0, size=(x,x));
+	return numpy.random.uniform(1.0, 5.0, size=(x,x));
 
 def calcularTiempos(option):
 	alg_iterativos_ = {'2':determinanteIter, '3':detRecursivo}
 	alg_numba_      = {'2':determinanteIterNumba, '3':detRecursivoNumba}
-	datos     = []
+	datos           = []
 
 	f1 = alg_iterativos_.get(option, None)
 	f2 = alg_numba_.get(option, None)
 	if f1 and f2:
-		max_ = input("Numero maximo de elementos: ")
-		min_ = input("Numero minimo de elementos: ")
-		i = input("Intervalo: ")
+		min_ = int(input("Numero minimo de elementos: "))
+		max_ = int(input("Numero maximo de elementos: "))
+		i    = int(input("Intervalo: "))
 
-		for x in range(min_,max_,i):
+		for x in range(min_,max_+1,i):
 			m = rellenaMatriz(x)
-
-			print(x, "elementos.")
 
 			# interprete
 			start_time = time()
@@ -55,6 +56,7 @@ def calcularTiempos(option):
 			f2(m, x)
 			tiempo_numba = time() - start_time
 
+			print(x, "elementos.", "Interprete:", '{:,.15f}'.format(tiempo_int), "segundos. Numba:", '{:,.15f}'.format(tiempo_numba), "segundos.")
 			datos.append([x, tiempo_int, tiempo_numba])
 
-#	escribeFichero(interprete_, str(f))
+	escribeFichero(datos, "prueba")
